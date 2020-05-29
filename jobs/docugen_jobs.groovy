8a064a22b5b3893800c90c9e435c828536c1c3ff
Gabriele Comoretto <gcomoretto@lsst.org>
2020-05-29 12:42:11 -0700

    Improved jobs steps

--- jobs/docugen_jobs.groovy
+++ jobs/docugen_jobs.groovy
@@ -1,9 +1,12 @@
-def gitUrl = 'https://github.com/lsst/LDM-639'
+// Docugeneration jobs
+
 
 script = '''#!/bin/bash
-git checkout jira-sync
+#git checkout jira-sync
+git pull
 
-docsteady generate-spec "/Data Management/Acceptance|LDM-639" jira_docugen.tex
+WORK_DIR=`pwd`
+source "${WORK_DIR}/.docugen"
 
 git diff
 git add jira_docugen.tex 
@@ -12,16 +15,20 @@ git add jira_imgs/
 git diff-index --quiet HEAD || git commit --author "Docsteady <noreply@lsst.org>" -m "Jenkins automatic update commit"
 '''
 
-folder('docs'){
-    description('All Docugen Jobs.')
-}
-folder('docs/DM'){
-    description('DM Docugen Jobs.')
-}
+
+folder('docs'){ description('All Docugen Jobs.') }
+folder('docs/DM'){ description('DM Docugen Jobs.') }
+folder('docs/SitCom'){ description('SitCom Docugen Jobs.') }
+
 
 job('docs/DM/LDM-639-docugen') {
     label('docugen')
+    def branch = "jira-sync"
+    def gitUrl = 'https://github.com/lsst/LDM-639'
     scm { git(gitUrl) }
     triggers { scm('H 4 * * 1-5') }
-    steps { shell(script) }
+    steps { 
+        shell('git checkout ' + branch)
+        shell(script)
+    }
 }
