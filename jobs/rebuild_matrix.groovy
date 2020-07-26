c75927bf795ec4c63cbcf06b196cc21ae4383d00
Gabriele Comoretto <gcomoretto@lsst.org>
2020-07-26 15:50:16 -0700

    fixes

---
+++ jobs/rebuild_matrix.groovy
@@ -0,0 +1,34 @@
+import javaposse.jobdsl.dsl.DslFactory
+import javaposse.jobdsl.dsl.Job
+
+def pipe_foler = "Pipelines"
+
+Job pipeline() {
+  folder("$pipe_folder"){}
+
+  dsl.pipelineJob("$pipe_folder/rebuild_matrix") {
+    keepDependencies()
+    concurrentBuild(false)
+
+    def repo = dsl.SEED_JOB.scm.userRemoteConfigs.get(0).getUrl()
+    def ref  = dsl.SEED_JOB.scm.getBranches().get(0).getName()
+
+    definition {
+      cpsScm {
+        scm {
+          git {
+            remote { url(repo) }
+            branch(ref)
+            extensions {
+              pathRestriction {
+                includedRegions(script())
+                excludedRegions(null)
+              }
+            }
+          }
+        }
+        "pipelines/rebuild-matrix.groovy"
+      }
+    }
+  }
+}
--- pipelines/rebuild_matrix.groovy
+++ pipelines/rebuild_matrix.groovy
@@ -1,22 +1,25 @@
+pipeline {
+  agent {
+    node {
+      label "master"
+    }
+  }
 
-def pipe_foler = "Pipelines"
-
-folder("$pipe_folder"){}
-
-
-pipeline("$pipe_folder/rebuild_matrix") {
+  def pipe_foler = "Pipelines"
 
   def arch_list = ["cos7", "cos8"]
 
   def matrix = [:]
 
-  stage('build') {
-    arch_list.each { arch ->
-      matrix[arch] = build(
-        job: "SciPipe/rebuild_" + arch,
-        parameters: [],
-        wait: true,
-      )
+  stages {
+    stage('build') {
+      arch_list.each { arch ->
+        matrix[arch] = build(
+          job: "SciPipe/rebuild_" + arch,
+          parameters: [],
+          wait: true,
+        )
+      }
     }
   }
 }
