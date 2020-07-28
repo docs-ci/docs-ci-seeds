a67385d47c52d7256f381d306e2b8a46c8cd1b5e
Gabriele Comoretto <gcomoretto@lsst.org>
2020-07-28 13:55:58 -0700

    fix11

--- jobs/rebuild_matrix.groovy
+++ jobs/rebuild_matrix.groovy
@@ -4,14 +4,7 @@ import javaposse.jobdsl.dsl.Job
 folder('Pipelines'){ description('All Pipelines Jobs.') }
 
 pipelineJob("Pipelines/rebuild_matrix") {
-  def String triggering = "H 4 0 0 0"
-  properties{
-      pipelineTriggers{
-        triggers {
-          cron(triggering)
-        }
-      }
-  }
+  properties([pipelineTriggers([cron('H 4 * * *')])])
 
   def repo = SEED_JOB.scm.userRemoteConfigs.get(0).getUrl()
   def ref  = SEED_JOB.scm.getBranches().get(0).getName()
