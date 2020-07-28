1de8b3fb8e4979ce516b3d6a927e5df6bd2d74b8
Gabriele Comoretto <gcomoretto@lsst.org>
2020-07-28 10:01:17 -0700

    Fix

--- pipelines/rebuild-matrix.groovy
+++ pipelines/rebuild-matrix.groovy
@@ -4,7 +4,7 @@ parallel {
   def arch_list = ["cos7", "cos8"]
   arch_list.each { arch ->
     stage('build' + arch) {
-        build( job: "SciPipe/rebuild_" + arch)
+        build(job: "SciPipe/rebuild_" + arch, parameters: [],)
     }
   }
 }
