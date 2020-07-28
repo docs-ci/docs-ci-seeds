bedc1eb35ccfb102cddf20632051140da2aad5bc
Gabriele Comoretto <gcomoretto@lsst.org>
2020-07-28 12:40:08 -0700

    Add trigger to rebuild_matrix pipeline

--- jobs/rebuild_matrix.groovy
+++ jobs/rebuild_matrix.groovy
@@ -5,6 +5,7 @@ folder('Pipelines'){ description('All Pipelines Jobs.') }
 
 pipelineJob("Pipelines/rebuild_matrix") {
   keepDependencies()
+  triggers { cron('H 4/* 0 0 0') }
 
   def repo = SEED_JOB.scm.userRemoteConfigs.get(0).getUrl()
   def ref  = SEED_JOB.scm.getBranches().get(0).getName()
