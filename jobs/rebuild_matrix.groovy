dfe60ae7cea4c6511070faea0d737f7a151df760
Gabriele Comoretto <gcomoretto@lsst.org>
2020-07-29 13:40:15 -0700

    Parametrize rebuilds

--- jobs/rebuild_matrix.groovy
+++ jobs/rebuild_matrix.groovy
@@ -4,7 +4,8 @@ import javaposse.jobdsl.dsl.Job
 folder('Pipelines'){ description('All Pipelines Jobs.') }
 
 pipelineJob("Pipelines/rebuild_matrix") {
-  //properties([cron('H 4 * * *')])
+  description('Execute a build of EUPS products using `lsstsw` on configured platforms.')
+
   properties {
     pipelineTriggers {
       triggers {
@@ -17,6 +18,14 @@ pipelineJob("Pipelines/rebuild_matrix") {
 
   def repo = SEED_JOB.scm.userRemoteConfigs.get(0).getUrl()
   def ref  = SEED_JOB.scm.getBranches().get(0).getName()
+
+  parameters {
+    stringParam('REFS', null, 'Whitespace delimited list of git references to pass to rebuild with -t option.')
+    stringParam('PRODUCTS', null, 'Whitespace delimited list of EUPS products to build.')
+    stringParam('SPLENV_REF', null, 'Conda env ref. If not specified it will use the default from lsstsw.')
+  }
+
+  concurrentBuild(true)
 
   definition {
     cpsScm {
--- jobs/rebuilds.groovy
+++ jobs/rebuilds.groovy
@@ -5,21 +5,34 @@ def jobs_dir = "Rebuilds"
 
 folder(jobs_dir){ description('Rebuild Jobs.') }
 
+
 arch_list.each { arch ->
   job("${jobs_dir}/rebuild-${arch}") {
     label(arch)
 
     parameters {
-      stringParam('REFS', null, 'Whitespace delimited list of git references to build.  Priority is highest -> lowest from left to right.  The "master" branch, or thebranch from repos.yaml, is implicitly appended to the right side of the list; do not specify "master" explicitly.')
-      stringParam('PRODUCTS', null,
-      'Whitespace delimited list of EUPS products to build.')
+      stringParam('REFS', null, 'Whitespace delimited list of git references to pass to rebuild with -t option')
+      stringParam('PRODUCTS', null, 'Whitespace delimited list of EUPS products to build.')
       stringParam('SPLENV_REF', null, 'Conda env ref. If not specified it will use the default from lsstsw.')
     }
 
     script = '#!/bin/bash\n' +
              'set +x\n\n' +
-             'source $JENKINS_HOME/' + arch + '/lsstsw/bin/envconfig\n\n' +
-             'rebuild lsst_distrib'
+             'REFS=$1\n' +
+             'PRODUCTS=$2\n' +
+             'SPLENV_REF=$3\n\n' +
+             'envref=""\n' +
+             'if [[ "${SPLENV_REF}" ]]; then\n' +
+             '  envref="-r ${SPLENV_REF}"\n' +
+             'fi\n' +
+             'source $JENKINS_HOME/' + arch + '/lsstsw/bin/envconfig $envref\n\n' +
+             'buildrefs=""\n' +
+             'if [[ "${REFS}" ]]; then\n' +
+             '  for r in ${REFS[*]}; do\n' +
+             '    buildrefs+=("-r $r")\n' +
+             '  done\n' +
+             'fi\n' +
+             'rebuild $buildrefs lsst_distrib'
     steps {
       shell(script)
     }
